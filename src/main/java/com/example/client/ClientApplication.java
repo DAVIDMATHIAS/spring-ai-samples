0530d1a9f17ec8ff687abfd1fb961bef4b913b67
davidmathias <thestardavid@gmail.com>
2025-04-10 18:31:02 +0530

    mcp client worked

--- src/main/java/com/example/client/ClientApplication.java
+++ src/main/java/com/example/client/ClientApplication.java
@@ -1,8 +1,14 @@
 package com.example.client;
 
+import io.modelcontextprotocol.client.McpClient;
+import io.modelcontextprotocol.client.McpSyncClient;
+import io.modelcontextprotocol.client.transport.ServerParameters;
+import io.modelcontextprotocol.client.transport.StdioClientTransport;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.mcp.SyncMcpToolCallbackProvider;
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,11 +17,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
 import java.util.concurrent.atomic.AtomicReference;
 
 @SpringBootApplication
 public class ClientApplication {
-	static final String FOLDER = "/home/davidmathias/test/user-context";
+	static final String FOLDER = "/home/davidmathias/work/mcp/mcp-client-demo-01/user-context";
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
 	}
@@ -23,23 +30,22 @@ public class ClientApplication {
 }
 
 @Configuration
-class LocalToolsAutoconfiguration {
-
-	@Component
-	static class Tools{
-
-		@Tool(description = "returns all the files in the "+ClientApplication.FOLDER+" folder")
-		String[] listFiles() {
-			System.out.println("Listing files in " + ClientApplication.FOLDER);
-			return new java.io.File(ClientApplication.FOLDER).list();
-		}
-
-	}
+class ThirdPartyConfiguration{
 	@Bean
-	NamedMCPClientRunner namedMCPClientRunner(ChatClient.Builder builder, Tools tools) {
+	NamedMCPClientRunner namedMCPClientRunner(ChatClient.Builder builder, McpSyncClient mcpSyncClient) {
+		var tools = new SyncMcpToolCallbackProvider(mcpSyncClient);
 		return new NamedMCPClientRunner(builder.defaultTools(tools));
 	}
-
+	@Bean
+	McpSyncClient mcpSyncClient(@Value(ClientApplication.FOLDER) File root) {
+		ServerParameters serverParameters = ServerParameters
+				.builder("npx")
+				.args("-y", "@modelcontextprotocol/server-filesystem", root.getAbsolutePath())
+				.build();
+		McpSyncClient mcp = McpClient.sync(new StdioClientTransport(serverParameters)).build();
+		mcp.initialize();
+		return mcp;
+	}
 }
 
 class NamedMCPClientRunner  implements ApplicationRunner, BeanNameAware {
